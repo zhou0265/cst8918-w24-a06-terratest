84403cfb2468d32d5e2399be4e9bb8195b489d07
zhou0265 <zhou0265@algonquinlive.com>
2025-02-14 23:47:21 -0500

    Added and fixed Terratest for Azure Linux VM

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "ee39387e-4892-429d-aeb2-ca4f142fbd25"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "zhou0265",
 		},
 	}
 
@@ -30,7 +30,18 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	// Run `terraform output` to get the value of output variable
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	nicName := terraform.Output(t, terraformOptions, "nic_name")
 
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// ✅ ADDITIONAL TEST 1: Confirm NIC exists and is connected to VM
+	nicExists := azure.NetworkInterfaceExists(t, subscriptionID, resourceGroupName, nicName)
+	assert.True(t, nicExists, "NIC should exist and be connected to the VM")
+
+	// ✅ ADDITIONAL TEST 2: Confirm the VM is running the correct Ubuntu version
+	vm := azure.GetVirtualMachine(t, subscriptionID, resourceGroupName, vmName)
+	assert.NotNil(t, vm.StorageProfile, "VM StorageProfile should not be nil")
+	assert.NotNil(t, vm.StorageProfile.ImageReference, "VM ImageReference should not be nil")
+	assert.Contains(t, *vm.StorageProfile.ImageReference.Sku, "22_04", "VM should be running Ubuntu 22.04")
 }
